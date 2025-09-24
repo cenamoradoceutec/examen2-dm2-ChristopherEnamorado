3b4dffe08227fe2773c797bd37852322bbfe1b1e
Christopher Enamorado <cenamorado@ceutec.edu>
2025-09-23 20:44:28 -0600

    tercercomit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQv-pHO8bVhXH1njXufJok2EHLc3Mqah0",
+    authDomain: "examen2suprimernombre.firebaseapp.com",
+    projectId: "examen2suprimernombre",
+    storageBucket: "examen2suprimernombre.firebasestorage.app",
+    messagingSenderId: "940793944860",
+    appId: "1:940793944860:web:5ad690ac5f43d7423c38d9"
+  },
 };
